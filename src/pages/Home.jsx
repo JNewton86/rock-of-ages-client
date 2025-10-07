8d7f7235bd96fc00a889379c55336e35a8330347
Jeff Newton <jeff@jnewton.pro>
2025-10-06 19:01:30 -0500

    testing ci/cd

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 function Home() {
   return (
-    <main className='text-slate-900 pl-10 pr-10'>
+    <main className='text-slate-900 pl-10 pr-9'>
       <h1 className='text-4xl'>Welcome to Rock of Ages</h1>
     </main>
   )
